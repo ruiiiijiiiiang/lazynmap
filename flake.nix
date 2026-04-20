3b0005536340f071c024876edd57a1a417d9ba46
Rui Jiang <me@ruijiang.me>
2026-04-20 11:44:10 -0500

    v0.1.2 release

--- flake.nix
+++ flake.nix
@@ -1,49 +1,5 @@
 {
-  description = "Lazynmap Nix Flake";
+  inputs.flakelight-rust.url = "github:accelbread/flakelight-rust";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-
-        lazynmapPkg = pkgs.rustPlatform.buildRustPackage {
-          pname = "lazynmap";
-          version = "0.1.1";
-
-          src = ./.;
-
-          cargoHash = "sha256-W9f3/jcd8WLWzf4d2QTYWuwdgCobtohKoK3bLvtuvQE=";
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          packages = [
-            pkgs.rust-bin.stable.latest.default
-          ];
-        };
-
-        packages.default = lazynmapPkg;
-
-        apps.default = {
-          type = "app";
-          program = "${lazynmapPkg}/bin/lazynmap";
-        };
-      }
-    );
+  outputs = { flakelight-rust, ... }: flakelight-rust ./. { };
 }
